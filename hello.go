0af0939bcd492a80cdb53e17ad5b13912f3ec14e
mostafijurr <mostafijur.rahman@brainstation23.com>
2021-07-18 11:17:39 +0600

    go learning

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+	var number int = 32
+	n2 := 56
+    number  = 68
+	fmt.Println(number, n2)
+}
---
+++ typeConversuib.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "strconv"
+//Import strconv package for String coversion
+
+func main() {
+
+	var integerNumber int = 56
+	fmt.Printf("%v \t %T \n", integerNumber, integerNumber)
+
+    var stringNumber  string
+	//this method is call to convert int to string
+	stringNumber  = strconv.Itoa(integerNumber)
+
+	fmt.Printf("%v \t %T \n", stringNumber, stringNumber)
+
+	var asciNumber int 
+	asciNumber = strconv.Atoi(stringNumber)
+
+	fmt.Printf("%v \t %T \n", asciNumber, asciNumber)
+
+
+}
