a863e1639d4905489f2dd3876d837faed725520f
mostafijurr <mostafijur.rahman@brainstation23.com>
2021-07-20 10:21:02 +0600

    functions

---
+++ functions.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Single instance and single return types
+func add(numberOne int, numberTwo int) int{
+	var result = numberOne+numberTwo
+	return result
+}
+
+//Multiple return type
+func multipleReturn(one, two int)(add, sub, multi, div int){
+	 add = one + two
+	 multi = one * two
+	 sub = one -two
+	 div = one / two
+	 return
+}
+
+/*
+	N.B: If you want to access any functions or variable outside of the package 
+	it names should start with capital latter, upper case or camel case. 
+*/
+
+func main(){
+
+	var sout  = add(5,6)
+	fmt.Println("Single return types result : ", sout)
+
+	var add,sub, multi, div = multipleReturn(1000,100)
+
+	fmt.Println("catches multiple instances")
+	fmt.Println("addition : ", add,"\t subtraction : ", sub, "\tmultiplication :", multi, "\tDivission : ", div)
+
+
+}
